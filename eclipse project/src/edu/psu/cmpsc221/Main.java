98cfe49cd41caa74d34e375d3773a92800202755
David McDermott <davidsamuelmcdermott@gmail.com>
2016-10-19 16:58:15 -0400

    Added the Eclipse project for the Application

---
+++ eclipse project/src/edu/psu/cmpsc221/Main.java
@@ -0,0 +1,18 @@
+package edu.psu.cmpsc221;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args){
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage){
+		primaryStage.setTitle("Media Player");
+		primaryStage.show();
+	}
+	
+}
